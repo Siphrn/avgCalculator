89813e4fa072ca34a1a6a7b2c2bb60cb41911810
Siphrn <lucaskordzakhia@yahoo.com>
2026-03-11 21:09:36 -0400

    Added comments for readability.

--- Grades.java
+++ Grades.java
@@ -1,8 +1,12 @@
+// Main function/declaring Grades Class
+
 public class Grades {
     public static void main(String[] args) {
+        // Declaring grades and avg. variables.
         int grades[] = {90, 100, 94, 85, 50};
         double avg = (grades[0] + grades[2] + grades[3] + grades[4] + grades[1]) / grades.length;
-        
+
+        // Conditional which goes through the grade, and outputs the Average: and Grade Letter: 
         if (avg > 90) {
             System.out.println("Average: " + avg);
             System.out.println("Grade: A");
