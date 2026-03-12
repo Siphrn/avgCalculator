5d501e6df01483bf29ede87b517ee04434484a23
Siphrn <lucaskordzakhia@yahoo.com>
2026-03-11 21:03:35 -0400

    Added the damn Code

---
+++ Grades.java
@@ -0,0 +1,30 @@
+public class Grades {
+    public static void main(String[] args) {
+        int grades[] = {90, 100, 94, 85, 50};
+        double avg = (grades[0] + grades[2] + grades[3] + grades[4] + grades[1]) / grades.length;
+        
+        if (avg > 90) {
+            System.out.println("Average: " + avg);
+            System.out.println("Grade: A");
+        }
+        else if (avg > 80) {
+            System.out.println("Average: " + avg);
+            System.out.println("Grade: B");
+        }
+        else if (avg > 70) {
+            System.out.println("Average: " + avg);
+            System.out.println("Grade: C");
+        }
+        else if (avg >= 60) {
+            System.out.println("Average: " + avg);
+            System.out.println("Grade: D");
+        }
+        else if (avg < 60) {
+            System.out.println("Average: " + avg);
+            System.out.println("Grade: F");
+        } else{
+            System.out.println("Not a valid average.");
+        }
+
+        }
+    }
